74e2d62ebfe869aa0a4d30aeb2e988f8f5d4397f
medhiwb <115658023+medhiwb@users.noreply.github.com>
2023-04-02 20:59:31 -0400

    stuff

--- readerwriter.c
+++ readerwriter.c
@@ -1,5 +1,15 @@
 #include "thread.h"
 
 int main () {
-    
+    char rw;
+    FILE *file = fopen("scenarios.txt", "r");
+
+    if (!file) {
+        printf("Error opening file.");
+        exit(1);
+    }
+
+    while(fscanf(file, "%c", &rw) != EOF) {
+        
+    }
 }
