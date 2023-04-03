95ff9976e2bde0287aee398cbda1c93e0c89d037
medhiwb <115658023+medhiwb@users.noreply.github.com>
2023-04-03 19:32:36 -0400

    s

--- readerwriter.c
+++ readerwriter.c
@@ -7,12 +7,12 @@ int main (int argc, char *argv[]) {
     char rw;
     // sem_init(&mutex, 0, 1);
     // sem_init(&homeboy, 0, 1);
-    // FILE *file = fopen("scenarios.txt", "r");
+    FILE *file = fopen("scenarios.txt", "r");
 
-    // if (!file) {
-    //     printf("Error opening file.");
-    //     exit(1);
-    // }
+    if (!file) {
+        printf("Error opening file.");
+        exit(1);
+    }
 
     // while(fscanf(file, "%c", &rw) != EOF) {
     //     int i;
@@ -26,14 +26,14 @@ int main (int argc, char *argv[]) {
     // sem_destroy(mutex);
     // sem_destroy(homeboy);
     int i = 0;
-    // while(fscanf(file, "%c", &rw) != EOF) {
-    //     do {
-    //         i++;
-    //     } while (rw == 'r');
+    while(fscanf(file, "%c", &rw) != EOF) {
+        while (rw == 'r') {
+            i++;
+        }
         
-    // }
-
+    }
+    fclose(file);
     printf("%d\n", i);
-    printf("hel");
+    printf("hel\n");
     return 0;
 }
