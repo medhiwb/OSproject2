5d92ea6d321120f0783586bac43b11881f6d11f4
medhiwb <115658023+medhiwb@users.noreply.github.com>
2023-04-03 19:00:45 -0400

    code not running

--- readerwriter.c
+++ readerwriter.c
@@ -1,21 +1,39 @@
 #include "thread.h"
 
-int main () {
+sem_t mutex;
+sem_t homeboy;
+
+int main (int argc, char *argv[]) {
     char rw;
-    FILE *file = fopen("scenarios.txt", "r");
+    // sem_init(&mutex, 0, 1);
+    // sem_init(&homeboy, 0, 1);
+    // FILE *file = fopen("scenarios.txt", "r");
 
-    if (!file) {
-        printf("Error opening file.");
-        exit(1);
-    }
+    // if (!file) {
+    //     printf("Error opening file.");
+    //     exit(1);
+    // }
 
-    while(fscanf(file, "%c", &rw) != EOF) {
-        int i;
-        if (rw == 'r') {
+    // while(fscanf(file, "%c", &rw) != EOF) {
+    //     int i;
+    //     if (rw == 'r') {
 
-        } else {
+    //     } else {
             
-        }
+    //     }
         
-    }
+    // }
+    // sem_destroy(mutex);
+    // sem_destroy(homeboy);
+    int i = 0;
+    // while(fscanf(file, "%c", &rw) != EOF) {
+    //     do {
+    //         i++;
+    //     } while (rw == 'r');
+        
+    // }
+
+    printf("%d\n", i);
+    printf("hel");
+    return 0;
 }
