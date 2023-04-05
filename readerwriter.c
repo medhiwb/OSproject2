849a37a46fd8158ecc2c8f20b581a41a975a6c7a
medhiwb <115658023+medhiwb@users.noreply.github.com>
2023-04-05 12:48:33 -0400

    bruh

--- readerwriter.c
+++ readerwriter.c
@@ -2,10 +2,18 @@
 
 #define MAX_READERS 100
 #define MAX_WRITERS 100
+#define MAXNUM 100
+#define MAX_RW 15
+
+Thread threadarray[100];
 
 int main (int argc, char *argv[]) {
     char rw;
-    Thread threadarray[100];
+    char filestr[MAXNUM];
+    char filestrings[MAXNUM][MAX_RW];
+    int numscenario = 0;
+
+    int filelen = 0;
     sem_init(&mutex, 0, 1);
     sem_init(&write_mutex, 0, 1);
 
@@ -17,20 +25,32 @@ int main (int argc, char *argv[]) {
         exit(1);
     }
 
-    int readcount = 0, i = 0;
-    while(fscanf(file, "%c", &rw) != EOF) {
-        if (rw == 'w') {
-            char* id = (char*)malloc(sizeof(char));
-            *id = 'w';
-            // pthread_create(&readers[i], NULL, reader, (void*)id);
-            threadarray[i++] = createThread(writer, id);
-        } else if (rw == 'r') {
-            char* id = (char*)malloc(sizeof(char));
-            *id = 'r';
-            // pthread_create(&writers[j], NULL, writer, (void*)id);
-            threadarray[i++] = createThread(reader, id);
-        }
+    while (fgets(filestr, MAXNUM, file) != NULL) {
+        //filestrings[numscenario] = malloc(strlen(filestr) + 1);
+        strcpy(&filestrings[numscenario], filestr);
+        numscenario++;
     }
+
+    for (int i = 0; i < numscenario; i++) {
+        printf("%s\n", filestrings[i]);
+        printf("%d\n", i);
+    }
+
+    int i = 0;
+    // while(fscanf(file, "%c", &rw) != EOF) {
+    //     if (rw == 'w') {
+    //         char* id = (char*)malloc(sizeof(char));
+    //         *id = 'w';
+    //         // pthread_create(&readers[i], NULL, reader, (void*)id);
+    //         threadarray[i++] = createThread(writer, id);
+    //     } else if (rw == 'r') {
+    //         char* id = (char*)malloc(sizeof(char));
+    //         *id = 'r';
+    //         // pthread_create(&writers[j], NULL, writer, (void*)id);
+    //         threadarray[i++] = createThread(reader, id);
+    //     }
+    // }
+    printf("%c", filestrings[0][1]);
 
     // for (int x = 0; x < i; x++) {
     //     pthread_join(readers[x], NULL);
@@ -40,10 +60,10 @@ int main (int argc, char *argv[]) {
     //     pthread_join(writers[y], NULL);
     // }
 
-    joinThreads(threadarray, i);
+    // joinThreads(threadarray, i);
 
     fclose(file);
-    sem_destroy(&mutex);
-    sem_destroy(&write_mutex);
+    // sem_destroy(&mutex);
+    // sem_destroy(&write_mutex);
     return 0;
 }
--- thread.h
+++ thread.h
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <string.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
